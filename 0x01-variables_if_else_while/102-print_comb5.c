83774e9f533f14343723c29d6c572fc1db7c6202
Idomason <idomakevin@gmail.com>
2022-03-14 20:27:28 -0700

    added a new file

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,45 +1,33 @@
 #include <stdio.h>
 /**
- * main - Prints numbers between 00 and 99.
- *
- * Return: Always 0 (Success)
+ * main - Prints all possible combinations of a pair of
+ * two digit numbers, without any repetition.
+(*
+ * Return: 0 on success.
  */
+
 int main(void)
 {
-	int i, e, g, h, op1, op2;
-	i = e = g = h = 48;
-	while (h < 58)
+	int i, j;
+
+	for (i = 0; i <= 99; i++)
 	{
-		g = 48;
-		while (g < 58)
+		for (j = 0; j <= 99; j++)
 		{
-			e = 48;
-			while (e < 58)
+			if (i < j && i != j)
 			{
-				i - 48;
-				while (i < 58)
+				putchar((i / 10) + '0');
+				putchar((i % 10) + '0');
+				putchar(' ');
+				putchar((j / 10) + '0');
+				putchar((j % 10) + '0');
+				if (i != 98 || j != 99)
 				{
-					op1 = (h * 10) + g;
-					op2 = (e * 10) + i;
-					if (op1 < op2)
-					{
-						putchar(h);
-						putchar(g);
-						putchar(' ');
-						putchar(e);
-						putchar(i);
-						if (h == 57 && g == 56 && e == 57 && i == 57)
-							break;
-						putchar(',');
-						putchar(' ');
-					}
-					i++;
+					putchar(',');
+					putchar(' ');
 				}
-				e++;
 			}
-			g++;
 		}
-		h++;
 	}
 	putchar('\n');
 	return (0);
